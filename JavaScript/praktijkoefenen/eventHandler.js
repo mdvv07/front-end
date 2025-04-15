7c0b958a698a611b295844d20f1e7faf90eb9a23
mdvv07 <clovavisuals@outlook.com>
2025-04-15 11:25:26 +0200

    Menukaart opdracht herschreven.
    ++

--- JavaScript/praktijkoefenen/eventHandler.js
+++ JavaScript/praktijkoefenen/eventHandler.js
@@ -2,10 +2,17 @@ var button = document.getElementById("create-button");
 console.log(button);
 
 
+var namelist =["Tim", "Escobar", "Tommy", "Bob", "Kevin"];
+
+for (var name in namelist){
+    button1 = document.createElement("button");
+    
+}
+
 button.addEventListener("click", createAnotherElement);
 
 function createAnotherElement() {
-    var newElement = document.createElement('h1');
-    newElement.textContent = 1 + `${0}`;
+    var newElement = document.createElement('img');
+    newElement.src = 'peak.jpg';
     document.body.appendChild(newElement);
 }
